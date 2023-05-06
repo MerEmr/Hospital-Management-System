cac41cdf289325c70d66977fde60c6a9702bf233
MERT UMUT YALPI <mertumut01@hotmail.com>
2023-05-06 16:50:25 +0300

    Admin Layout olusturuldu linkler baglandi.Admin Layout css dosya yollari duzenlenecek.Vıewler bitti.

---
+++ HospitalManagementSystem/Controllers/DoctorController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HospitalManagementSystem.UI.Controllers
+{
+    public class DoctorController : Controller
+    {
+        public IActionResult DoctorList()
+        {
+            return View();
+        }
+        public IActionResult DoctorOperations()
+        {
+            return View();
+        }
+    }
+}
---
+++ HospitalManagementSystem/Controllers/PersonalController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HospitalManagementSystem.UI.Controllers
+{
+    public class PersonalController : Controller
+    {
+        public IActionResult PersonalList()
+        {
+            return View();
+        }
+
+        public IActionResult PersonalOperations()
+        {
+
+            return View();
+        }
+    }
+}
